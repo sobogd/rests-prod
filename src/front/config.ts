90b8de502fb47474958ef373d51dfff331b28dfe
sobogd <sobogd@gmail.com>
2023-11-26 12:05:42 +0300

    fix

--- src/front/config.ts
+++ src/front/config.ts
@@ -1,4 +1,4 @@
 // export const API_URL = `https://server.rests.app`;
-export const API_URL = `http://localhost:4000`;
+export const API_URL = `/`;
 
 export const IMAGE_URL: string = "https://storage.googleapis.com/rests-files/";
