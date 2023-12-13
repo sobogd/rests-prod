534955417c33f8b3c19ca70eb3b3e75a74745429
sobogd <sobogd@gmail.com>
2023-12-13 15:39:23 +0300

    fix upload google

--- src/back/config.ts
+++ src/back/config.ts
@@ -7,7 +7,7 @@ export const storage = new Storage({
   projectId: process.env.GCLOUD_SCHEME || "",
   credentials: {
     type: "service_account",
-    private_key: process.env.GCLOUD_PRIVATE_KEY || "",
+    private_key: process.env.GCLOUD_PRIVATE_KEY?.split(String.raw`\n`).join("\n") || "",
     client_email: process.env.GCLOUD_CLIENT_EMAIL || "",
     client_id: process.env.GCLOUD_CLIENT_ID || "",
     universe_domain: "googleapis.com",
