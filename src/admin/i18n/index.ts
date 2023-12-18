fdd09bb13b27c145d5fa1e75f08fa55cd329a2cf
sobogd <sobogd@gmail.com>
2023-12-19 00:23:57 +0300

    tr lang

--- src/admin/i18n/index.ts
+++ src/admin/i18n/index.ts
@@ -2,6 +2,7 @@ import { createInstance } from "i18next";
 import { initReactI18next } from "react-i18next";
 import i18nRu from "./ru";
 import i18nEn from "./en";
+import i18nTr from "./tr";
 
 const i18n = createInstance({
   fallbackLng: "en",
@@ -11,6 +12,9 @@ const i18n = createInstance({
     },
     en: {
       translation: i18nEn,
+    },
+    tr: {
+      translation: i18nTr,
     },
   },
 });
